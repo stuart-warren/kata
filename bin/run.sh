9c80f15929d96926e356bf62166f4ac18535ca61
stuart warren <stuartwarren@ntlworld.com>
2017-06-07 19:57:18 +0100

    commit Wed  7 Jun 2017 19:57:18 BST

--- bin/run.sh
+++ bin/run.sh
@@ -3,7 +3,7 @@
 ## brew install fswatch
 while true
 do
-  fswatch --one-event -iE ".*\.[go]+" . && \
+  fswatch --one-event -iE ".*\.go$" . && \
   go test ./... && git add . && \
   git commit -m "commit $(date)" && \
   echo "success \o/"
