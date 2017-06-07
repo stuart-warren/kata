26a01a174d4554f6a78c9ae1a04562924bc3c9cf
stuart warren <stuartwarren@ntlworld.com>
2017-06-07 18:58:56 +0100

    commit Wed  7 Jun 2017 18:58:56 BST

--- fizzbuzz/fizzbuzz.go
+++ fizzbuzz/fizzbuzz.go
@@ -2,6 +2,11 @@ package fizzbuzz
 
 import "strconv"
 
+// Do takes a Number
+// if number devides by 3, output fizz
+// if number divides by 5, output buzz
+// if number divides by 3 & 5, ouput fizzbuzz
+// else output the number
 func Do(input int) (string, error) {
 	return strconv.Itoa(input), nil
 }
